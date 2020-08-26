87174d43e59272841cbc6ccb0b248f8dc983867d
bridget462 <bridget.dev.create@gmail.com>
2020-08-25 18:49:49 -0700

    screen API used to implement picture in picture function with START button

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    // screen capture API Docs: https://developer.mozilla.org/en-US/docs/Web/API/Screen_Capture_API/Using_Screen_Capture
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("whoops, error here:", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disabled = true;
+  // Start Picture in Picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disabled = false;
+});
+
+// on Load
+selectMediaStream();
